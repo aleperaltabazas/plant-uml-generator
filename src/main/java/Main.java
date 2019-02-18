bf0bc7e9303470fd024875905ca65d19c3fbe392
Alejandro <aleperaltabazas@hotmail.com>
2019-02-18 00:39:22 -0300

    trying to build a package

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,12 @@
 import some.Baz;
 import some.more.Foo;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Set;
 
 public class Main {
@@ -8,9 +14,22 @@ public class Main {
         ClassFinder finder = new ClassFinder();
         ClassReader reader = new ClassReader();
 
+        ClassLoader loader = Thread.currentThread().getContextClassLoader();
+
         Set<Class<?>> classes = finder.findClasses(Baz.class.getPackage());
         FinalButNotReallyString uml = new FinalButNotReallyString();
         classes.forEach(c -> uml.concat(reader.read(c)));
+
+        try {
+            Constructor<Package> packageConstructor = Package.class.getDeclaredConstructor(String.class, String.class, String.class, String.class, String.class, String.class, String.class, URL.class, ClassLoader.class);
+            packageConstructor.setAccessible(true);
+            Package instance = packageConstructor.newInstance("some", null, null, null, null, null, null, null, Thread.currentThread().getContextClassLoader());
+            System.out.println(instance.getName());
+            System.out.println(Baz.class.getPackage().getName());
+        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException | InstantiationException e) {
+            e.printStackTrace();
+        }
+
 
         System.out.println(uml.getText());
     }
