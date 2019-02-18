2a71c27e757a814b8e92356684fce22e677d2ee0
Alejandro <aleperaltabazas@hotmail.com>
2019-02-18 18:09:37 -0300

    finished?

--- src/main/java/ClassFinder.java
+++ src/main/java/ClassFinder.java
@@ -5,11 +5,37 @@ import org.reflections.util.ClasspathHelper;
 import org.reflections.util.ConfigurationBuilder;
 import org.reflections.util.FilterBuilder;
 
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Set;
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.*;
 
 public class ClassFinder {
+    public void loadClassesInDirectory(String directory) {
+        File dir = new File(directory);
+
+        if (!dir.isDirectory()) {
+            throw new RuntimeException("Should be a directory");
+        }
+
+        List<File> files = Arrays.asList(dir.listFiles());
+        files.forEach(f -> {
+            if (f.isDirectory())
+                loadClassesInDirectory(f.getName());
+        });
+
+        try {
+            URL url = dir.toURI().toURL();
+            URL[] urls = new URL[]{url};
+
+            ClassLoader cl = new URLClassLoader(urls);
+
+            cl.loadClass(dir.getName().replace('/', '.'));
+        } catch (MalformedURLException | ClassNotFoundException e) {
+        }
+    }
+
     public Set<Class<?>> findClasses(Package _package) {
         List<ClassLoader> classLoadersList = new LinkedList<ClassLoader>();
         classLoadersList.add(ClasspathHelper.contextClassLoader());
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,36 +1,41 @@
-import some.Baz;
-import some.more.Foo;
-
-import java.lang.reflect.Constructor;
-import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
-import java.net.URL;
-import java.util.Arrays;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
-        ClassFinder finder = new ClassFinder();
-        ClassReader reader = new ClassReader();
-
-        ClassLoader loader = Thread.currentThread().getContextClassLoader();
-
-        Set<Class<?>> classes = finder.findClasses(Baz.class.getPackage());
-        FinalButNotReallyString uml = new FinalButNotReallyString();
-        classes.forEach(c -> uml.concat(reader.read(c)));
+        String directory = args[0];
+        String diagramName;
 
         try {
-            Constructor<Package> packageConstructor = Package.class.getDeclaredConstructor(String.class, String.class, String.class, String.class, String.class, String.class, String.class, URL.class, ClassLoader.class);
-            packageConstructor.setAccessible(true);
-            Package instance = packageConstructor.newInstance("some", null, null, null, null, null, null, null, Thread.currentThread().getContextClassLoader());
-            System.out.println(instance.getName());
-            System.out.println(Baz.class.getPackage().getName());
-        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException | InstantiationException e) {
+            diagramName = args[1];
+        } catch (Exception e) {
+            diagramName = "class-diagram.txt";
+        }
+
+        ClassFinder classFinder = new ClassFinder();
+        PackageFinder packageFinder = new PackageFinder();
+        ClassReader reader = new ClassReader();
+
+        classFinder.loadClassesInDirectory(directory);
+        List<Package> packages = packageFinder.packagesFrom(directory);
+        Set<Class<?>> classes = new HashSet<>();
+        packages.forEach(p -> classes.addAll(classFinder.findClasses(p)));
+
+        FinalButNotReallyString uml = new FinalButNotReallyString();
+        classes.forEach(c -> uml.concat(reader.read(c) + "\n"));
+
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(diagramName));
+            writer.write(uml.getText());
+            writer.close();
+        } catch (IOException e) {
             e.printStackTrace();
         }
 
 
-        System.out.println(uml.getText());
     }
 }
