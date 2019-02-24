5cf0619893754f0298604be77f2b5cdd2e592ccc
Alejandro <aleperaltabazas@hotmail.com>
2019-02-24 18:48:53 -0300

    adding FileManager

--- src/main/java/FileManager.java
+++ src/main/java/FileManager.java
@@ -1,2 +1,41 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class FileManager {
+    public List<String> findAllClasses(String path) {
+        File file = new File(path);
+        List<File> content = Arrays.asList(file.listFiles());
+
+        List<String> classes = new ArrayList<>();
+
+        content.forEach(f -> {
+            if (f.isDirectory())
+                findAllClasses(f.getAbsolutePath());
+            else if (f.getName().endsWith(".java")) {
+                try {
+                    classes.add(fileToText(f.getAbsolutePath()));
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        return classes;
+    }
+
+    public String fileToText(String path) throws FileNotFoundException {
+        BufferedReader reader = new BufferedReader(new FileReader(path));
+        List<String> lines = reader.lines().collect(Collectors.toList());
+
+        StringBuilder sb = new StringBuilder();
+        lines.forEach(line -> sb.append(line).append("\n"));
+
+        return sb.toString();
+    }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,36 +1,5 @@
-import klass.Klass;
-
 public class Main {
-    public static void main(String[] args) throws Exception {
-        String foo = "public class Foo {\n" +
-                "   private int bar;\n" +
-                "   public int getBar() {\n" +
-                "       return bar;\n" +
-                "   }\n" +
-                "   public void doSomething(String something) {\n" +
-                "       System.out.println(\"Hello World !\");\n" +
-                "   }\n" +
-                "}";
+    public static void main(String[] args) {
 
-        KlassReader reader = new KlassReader();
-        Klass klass = reader.readKlass(foo);
-
-        System.out.println(klass.getName());
-        System.out.println(klass.getParent());
-        System.out.println(klass.getClassType());
-
-        System.out.println("\nATTRIBUTES\n");
-
-        klass.getAttributes().forEach(at -> {
-            System.out.println(at.getName());
-            System.out.println(at.getKlass());
-        });
-
-        System.out.println("\nMETHODS\n");
-
-        klass.getMethods().forEach(m -> {
-            System.out.println(m.getReturnType());
-            System.out.println(m.getName());
-        });
     }
 }
