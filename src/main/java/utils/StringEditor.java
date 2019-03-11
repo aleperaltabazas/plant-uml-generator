f0ebb79b8ebca995d652dddcc63d875832b78da1
Alejandro <aleperaltabazas@hotmail.com>
2019-03-11 17:02:59 -0300

    making the generic unwrapping recursive

--- src/main/java/utils/StringEditor.java
+++ src/main/java/utils/StringEditor.java
@@ -1,11 +1,17 @@
 package utils;
 
 public class StringEditor {
+    private static final String genericRegex = "\\w+<.*>";
+
     public static String removeListWrapper(String str) {
         int lastIndexOf = str.lastIndexOf('>');
         StringBuilder sb = new StringBuilder(str);
         sb.replace(lastIndexOf, lastIndexOf + 1, "");
 
-        return sb.toString().replaceFirst("<", "").replaceFirst("(List|Set|Map)", "");
+        String unWrapped = sb.toString().replaceFirst("<", "").replaceFirst("(List|Set|Map)", "");
+        if (unWrapped.matches(genericRegex))
+            return removeListWrapper(unWrapped);
+
+        return unWrapped;
     }
 }
